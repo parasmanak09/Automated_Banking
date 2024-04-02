e62f07d6fa7a0aad43197728b201d0cea1b5008c
Paras Manak <parasmanak09@gmail.com>
2024-04-02 22:13:39 +0530

    Add files via upload

---
+++ Dashboard/src/page/index.jsx
@@ -0,0 +1,266 @@
+import React from 'react';
+import './index.css';
+
+export default function Main() {
+  return (
+    <div className='main-container'>
+      <div className='root'>
+        <div className='image'>
+          <div className='groups'>
+            <div className='image-1' />
+            <span className='dashboard'>Dashboard</span>
+            <span className='nicola-rich'>Ram</span>
+            <div className='image-2' />
+            <div className='image-3' />
+          </div>
+          <div className='image-4'>
+            <div className='groups-5'>
+              <div className='flex-row-ec'>
+                <span className='transactions'>Transactions</span>
+                <div className='button'>
+                  <div className='background'>
+                    <button className='view-all' />
+                  </div>
+                </div>
+              </div>
+              <div className='flex-row-b'>
+                <span className='expenses'>Expenses</span>
+                <span className='income'>Income</span>
+                <div className='image-6' />
+              </div>
+            </div>
+            <div className='groups-7'>
+              <div className='flex-row-f'>
+                <div className='image-8' />
+                <span className='central-burger'>Central Burger</span>
+                <span className='negative-amount'>-$189.36</span>
+              </div>
+              <span className='cafe-restaurant'>Cafe and Restaurant</span>
+            </div>
+            <div className='groups-9'>
+              <div className='flex-column-d'>
+                <span className='the-market'>The Market</span>
+                <span className='groceries'>Groceries</span>
+              </div>
+              <div className='image-a' />
+              <span className='negative-amount-b'>-$92.50</span>
+            </div>
+            <div className='groups-c'>
+              <div className='image-d'>
+                <div className='flex-column-ac'>
+                  <span className='quick-transfer'>QuickTransfer</span>
+                  <span className='maria-purple'>Maria Purple</span>
+                </div>
+                <div className='image-e' />
+                <span className='market-price'>+$350.0</span>
+              </div>
+            </div>
+            <div className='groups-f'>
+              <div className='image-10'>
+                <div className='flex-column-d-11'>
+                  <span className='the-market-12'>The Market</span>
+                  <span className='groceries-13'>Groceries</span>
+                </div>
+                <div className='image-14' />
+                <span className='negative-price'>-$36.20</span>
+              </div>
+            </div>
+            <div className='groups-15'>
+              <div className='flex-column'>
+                <span className='central-burger-16'>Central Burger</span>
+                <span className='cafe-restaurant-17'>Cafe and Restaurant</span>
+              </div>
+              <div className='image-18' />
+              <span className='negative-price-19'>-$189.36</span>
+            </div>
+            <div className='groups-1a'>
+              <div className='image-1b'>
+                <span className='quick-transfer-1c'>Quick Transfer</span>
+                <div className='flex-row-f-1d'>
+                  <div className='groups-1e'>
+                    <div className='image-1f' />
+                    <span className='add'>Add</span>
+                    <span className='new'>New</span>
+                  </div>
+                  <div className='groups-20'>
+                    <div className='image-21' />
+                    <span className='maria'>Maria</span>
+                    <span className='purple'>Purple</span>
+                  </div>
+                  <div className='groups-22'>
+                    <div className='image-23' />
+                    <span className='lepnard'>Lepnard</span>
+                    <span className='smit'>Smit</span>
+                  </div>
+                  <div className='groups-24'>
+                    <div className='image-25' />
+                    <span className='osoar'>Osoar</span>
+                    <span className='wild'>wild</span>
+                  </div>
+                  <div className='groups-26'>
+                    <div className='image-27' />
+                    <span className='karen'>Karen</span>
+                    <span className='potter'>Potter</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='groups-28'>
+            <div className='image-29'>
+              <span className='total-balance'>Total Balance</span>
+              <div className='flex-row-ea'>
+                <div className='image-2a' />
+                <span className='percentage'>2,36%</span>
+              </div>
+              <span className='usd-inr'>INR 10,000.00</span>
+              <span className='income-2b'>Income</span>
+              <div className='flex-row'>
+                <span className='expense'>Expense</span>
+                <div className='image-2c' />
+                <div className='image-2d' />
+              </div>
+              <span className='usd-inr-2e'>INR 20,000.00</span>
+              <span className='usd-inr-2f'>INR 30,000.00</span>
+            </div>
+          </div>
+          <div className='image-30'>
+            <div className='flex-row-a'>
+              <span className='total-savings'>Total Savings</span>
+              <div className='image-31' />
+              <span className='percentage-32'>2,36%</span>
+            </div>
+            <span className='usd-inr-33'>INR 5,000.00</span>
+            <div className='image-34'>
+              <div className='image-35' />
+              <div className='image-36' />
+              <div className='image-37' />
+              <div className='image-38' />
+            </div>
+          </div>
+          <div className='groups-39'>
+            <div className='image-3a'>
+              <div className='flex-row-edf'>
+                <button className='button-3b'>
+                  <div className='background-3c'>
+                    <span className='last-7-days'>Last 7 days</span>
+                    <div className='image-3d' />
+                  </div>
+                </button>
+                <span className='statistics'>Statistics</span>
+                <span className='expenses-3e'>Expenses</span>
+                <span className='income-3f'>Income</span>
+                <div className='image-40' />
+              </div>
+              <div className='flex-row-ad'>
+                <div className='image-41' />
+                <span className='k-3k'>3K</span>
+                <span className='k-2k'>2K</span>
+                <span className='k-1k'>1K</span>
+                <span className='zero'>0</span>
+              </div>
+              <div className='flex-row-e'>
+                <span className='slash-1-4'>1/4</span>
+                <span className='slash-1-1'>1/1</span>
+                <span className='slash-1-2'>1/2</span>
+                <span className='slash-1-3'>1/3</span>
+                <span className='slash-1-5'>1/5</span>
+                <span className='slash-1-6'>1/6</span>
+                <span className='text-38'>17</span>
+              </div>
+            </div>
+          </div>
+          <div className='groups-42'>
+            <div className='image-43'>
+              <div className='flex-column-a'>
+                <span className='summer-vacation'>Summer Vacation</span>
+                <span className='out-of-s-1485-out-of-s2385'>
+                  $1,485 out of S2385
+                </span>
+                <span className='goals'>Goals</span>
+                <div className='image-44' />
+                <div className='image-45' />
+                <div className='image-46' />
+              </div>
+              <span className='percent-reached'>62%reached</span>
+            </div>
+          </div>
+          <div className='groups-47'>
+            <div className='image-48'>
+              <span className='spending-overview'>Spending Overview</span>
+              <div className='flex-row-b-49'>
+                <div className='image-4a'>
+                  <div className='image-4b' />
+                </div>
+                <div className='group-d'>
+                  <span className='percent'>68%</span>
+                  <span className='groceries-4c'>Groceries</span>
+                </div>
+              </div>
+              <div className='flex-row-'>
+                <div className='image-4d'>
+                  <div className='image-4e' />
+                </div>
+                <span className='percent-4f'>20%</span>
+                <span className='withdrawal'>Withdrawal</span>
+              </div>
+              <div className='flex-row-a-50'>
+                <div className='image-51'>
+                  <div className='image-52' />
+                  <div className='image-53' />
+                </div>
+                <div className='regroup'>
+                  <span className='percentage-54'>10%</span>
+                  <span className='retail'>Retail</span>
+                </div>
+              </div>
+              <div className='flex-row-cc'>
+                <div className='text'>
+                  <div className='background-55'>
+                    <span className='text-56' />
+                  </div>
+                </div>
+                <span className='percentage-57'>2%</span>
+                <span className='leisure'>Leisure</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='image-58'>
+          <div className='image-59' />
+          <div className='flex-column-a-5a'>
+            <span className='settings'>Settings</span>
+            <span className='log-out'>Log out</span>
+          </div>
+          <div className='flex-column-df'>
+            <div className='image-5b' />
+            <div className='image-5c' />
+          </div>
+        </div>
+        <div className='groups-5d'>
+          <div className='groups-5e'>
+            <span className='plutus'>Plutus</span>
+            <div className='flex-row-d'>
+              <div className='image-5f' />
+              <span className='dashboard-60'>Dashboard</span>
+            </div>
+            <div className='flex-row-f-61'>
+              <div className='image-62' />
+              <span className='transfer'>Transfer</span>
+            </div>
+            <div className='flex-row-be'>
+              <div className='image-63' />
+              <span className='transaction'>Transaction</span>
+            </div>
+            <div className='flex-row-eae'>
+              <div className='image-64' />
+              <span className='accounts-and-cards'>Accounts and Cards</span>
+            </div>
+            <span className='investments'>Investments</span>
+            <div className='image-65' />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
