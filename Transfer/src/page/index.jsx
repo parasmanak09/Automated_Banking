ca4dec452613dd8dbf1294e2414f3aeaefdaa5e3
Paras Manak <parasmanak09@gmail.com>
2024-04-02 22:14:43 +0530

    Add files via upload

---
+++ Transfer/src/page/index.jsx
@@ -0,0 +1,181 @@
+import React from 'react';
+import './index.css';
+
+export default function Main() {
+  return (
+    <div className='main-container'>
+      <div className='root'>
+        <div className='image' />
+        <div className='image-1'>
+          <div className='flex-column-e'>
+            <div className='groups'>
+              <div className='image-2' />
+              <span className='transfer'>Transfer</span>
+              <span className='nicola-rich'>Nicola Rich</span>
+              <div className='image-3' />
+              <div className='image-4' />
+            </div>
+            <div className='groups-5'>
+              <span className='transfer-to'>Transfer to</span>
+              <div className='flex-row-cff'>
+                <span className='own-account'>Own account</span>
+                <span className='other-account'>Other account</span>
+              </div>
+              <div className='image-6' />
+              <span className='beneficiary'>Beneficiary</span>
+              <span className='iban'>IBAN *</span>
+              <div className='image-7'>
+                <span className='usd'>USD 0.00</span>
+                <span className='amount'>Amount *</span>
+              </div>
+              <div className='text'>
+                <div className='background'>
+                  <span className='date'>Date *</span>
+                  <div className='image-8' />
+                </div>
+              </div>
+              <div className='groups-9'>
+                <div className='flex-row-f'>
+                  <span className='other-data'>Other data</span>
+                  <div className='image-a' />
+                </div>
+                <div className='flex-row'>
+                  <div className='image-b'>
+                    <span className='transfer-purpose'>Transfer purpose</span>
+                  </div>
+                  <div className='image-c'>
+                    <span className='beneficiarys-name'>
+                      Beneficiary's name
+                    </span>
+                  </div>
+                </div>
+                <div className='flex-row-eb'>
+                  <div className='image-d'>
+                    <span className='beneficiarys-email'>
+                      Beneficiary's email
+                    </span>
+                  </div>
+                  <div className='image-e'>
+                    <span className='payers-reference'>Payer's reference</span>
+                  </div>
+                </div>
+                <div className='text-f'>
+                  <div className='image-10'>
+                    <div className='background-11'>
+                      <span className='information-for-beneficiary'>
+                        Information for beneficiary
+                      </span>
+                    </div>
+                  </div>
+                </div>
+                <div className='button'>
+                  <button className='button-background'>
+                    <span className='continue'>Continue</span>
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div className='groups-12'>
+              <div className='image-13'>
+                <div className='flex-row-b'>
+                  <span className='saved-beneficiaries'>
+                    Saved Beneficiaries
+                  </span>
+                  <span className='view-all'>View all</span>
+                </div>
+                <div className='flex-row-aec'>
+                  <div className='groups-14'>
+                    <div className='image-15' />
+                    <span className='add'>Add</span>
+                    <span className='new'>New</span>
+                  </div>
+                  <div className='groups-16'>
+                    <div className='image-17' />
+                    <span className='maria-purple'>
+                      Maria
+                      <br />
+                      Purple
+                    </span>
+                  </div>
+                  <div className='groups-18'>
+                    <div className='image-19' />
+                    <span className='leonard-smith'>
+                      Leonard
+                      <br />
+                      Smith
+                    </span>
+                  </div>
+                  <div className='groups-1a'>
+                    <div className='image-1b' />
+                    <span className='osca-wild'>
+                      Osca
+                      <br />
+                      Wild
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='groups-1c'>
+            <span className='select-payer'>Selectpayer</span>
+            <div className='image-1d'>
+              <span className='checking-account'>Checking Account</span>
+              <div className='image-1e' />
+            </div>
+            <div className='image-1f'>
+              <span className='checking-account-20'>Checking Account</span>
+              <span className='balance'>Balance</span>
+              <span className='usd-inr'>INR 10,000.00</span>
+              <div className='image-21' />
+              <span className='iban-22'>Account Number</span>
+              <span className='ab'>AB110000000011111111</span>
+              <span className='account-owner'>Account owner</span>
+              <span className='nicola-rich-23'>Ram</span>
+            </div>
+            <div className='button-24'>
+              <div className='image-25'>
+                <div className='background-26'>
+                  <span className='share-iban'>Share Acc No</span>
+                  <div className='image-27' />
+                </div>
+              </div>
+            </div>
+            <div className='image-28'>
+              <span className='request-payment'>Request payment</span>
+              <div className='image-29' />
+            </div>
+          </div>
+        </div>
+        <div className='groups-2a'>
+          <div className='flex-column-bd'>
+            <div className='groups-2b'>
+              <span className='plutus'>Plutus</span>
+              <span className='dashboard'>Dashboard</span>
+              <div className='image-2c' />
+              <div className='groups-2d'>
+                <span className='transfer-2e'>Transfer</span>
+                <div className='image-2f' />
+              </div>
+              <div className='groups-30'>
+                <span className='transaction'>Transaction</span>
+                <div className='image-31' />
+              </div>
+              <div className='groups-32'>
+                <span className='accounts-and-cards'>Accounts and Cards</span>
+                <div className='image-33' />
+              </div>
+              <span className='investments'>Investments</span>
+              <div className='image-34' />
+            </div>
+            <span className='settings'>Settings</span>
+            <div className='image-35' />
+            <span className='log-out'>Log out</span>
+            <div className='image-36' />
+          </div>
+          <div className='image-37' />
+        </div>
+      </div>
+    </div>
+  );
+}
