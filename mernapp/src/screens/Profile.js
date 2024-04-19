e200c5cd9dfe3da775ff365a1eda97d80391a3a6
Paras Manak <parasmanak09@gmail.com>
2024-04-19 19:42:26 +0530

    Update Profile.js

--- mernapp/src/screens/Profile.js
+++ mernapp/src/screens/Profile.js
@@ -1,21 +1,45 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import '../components/Home.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import '../components/Background.js' // IMport JS Background File
-import Background from '../components/Background.js'
+import React from 'react';
+import Navbar from '../components/Navbar';
+import Background from '../components/Background.js'; // Import Background component correctly
+import '../components/Home.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../components/profile.css';
+
 export default function Profile() {
-    return (<>
-        <Navbar/>
-        <Background/>
-
-
-
-
-
-
-     
-        </>
-
-    );
+  return (
+    <div>
+      <Navbar />
+      <Background />
+      <div className="container rounded bg-white mt-5 mb-5">
+        <div className="row">
+          <div className="col-md-3 border-right">
+            <div className="d-flex flex-column align-items-center text-center p-3 py-5">
+              <img className="rounded-circle mt-5" src="https://i.kinja-img.com/gawker-media/image/upload/gd8ljenaeahpn0wslmlz.jpg" alt="profile-img" />
+            </div>
+          </div>
+          <div className="col-md-5 border-right">
+            <div className="p-3 py-5">
+              <div className="d-flex justify-content-between align-items-center mb-3">
+                <h4 className="text-right">Profile Settings</h4>
+              </div>
+              <div class="row mt-2">
+                    <div class="col-md-6"><label class="labels">Name</label><input type="text" class="form-control" placeholder="first name" value=""></input></div>
+                    <div class="col-md-6"><label class="labels">Surname</label><input type="text" class="form-control" value="" placeholder="surname"></input></div>
+                </div>
+                <div class="row mt-3">
+                    <div class="col-md-12"><label class="labels">Mobile Number</label><input type="text" class="form-control" placeholder="enter phone number" value=""></input></div>
+                    <div class="col-md-12"><label class="labels">Address Line 1</label><input type="text" class="form-control" placeholder="enter address line 1" value=""></input></div>
+                    <div class="col-md-12"><label class="labels">Address Line 2</label><input type="text" class="form-control" placeholder="enter address line 2" value=""></input></div>
+                    <div class="col-md-12"><label class="labels">Postcode</label><input type="text" class="form-control" placeholder="enter address line 2" value=""></input></div>
+                    <div class="col-md-12"><label class="labels">State</label><input type="text" class="form-control" placeholder="enter address line 2" value=""></input></div>
+                    <div class="col-md-12"><label class="labels">Area</label><input type="text" class="form-control" placeholder="enter address line 2" value=""></input></div>
+                    <div class="col-md-12"><label class="labels">Email ID</label><input type="text" class="form-control" placeholder="enter email id" value=""></input></div>
+                    <div class="col-md-12"><label class="labels">Education</label><input type="text" class="form-control" placeholder="education" value=""></input></div>
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
